26db61281947d965c86567d0cf97a479b3e440a2
futchitwo <74236683+futchitwo@users.noreply.github.com>
2022-11-09 22:25:22 +0900

    [encore fix] fix typo

--- internal/typeutils/defaulticons.go
+++ internal/typeutils/defaulticons.go
@@ -51,7 +51,7 @@ func populateDefaultAvatars() (defaultAvatars []string) {
 	defaultAvatarFiles, err := os.ReadDir(defaultAvatarsAbsFilePath)
 	*/
 
-	items, err := web.WebFS.ReadDir("assets/default_avatars")
+	defaultAvatarFiles, err := web.WebFS.ReadDir("assets/default_avatars")
 	if err != nil {
 		log.Warnf("populateDefaultAvatars: error reading default avatars at %s: %s", defaultAvatarsAbsFilePath, err)
 		return
