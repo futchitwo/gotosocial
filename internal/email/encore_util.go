abfa43149fda4460df1ffdc8e8355f803d2a95b2
futchitwo <74236683+futchitwo@users.noreply.github.com>
2022-11-20 02:37:07 +0900

    [encore] Use embedFS as email template dir

---
+++ internal/email/encore_util.go
@@ -0,0 +1,19 @@
+package email
+
+import (
+	"io/fs"
+	"html/template"
+
+ 	"github.com/superseriousbusiness/gotosocial/web/embed"
+)
+
+func loadTemplatesFromEmbed() (*template.Template, error) {
+	tmplDir, err := fs.Sub(embed.WebFS, "template")
+ 	if err != nil {
+ 		return err
+ 	}
+
+ 	tmpl := template.New("").Funcs(funcMap)
+	// look for all templates that start with 'email_'
+ 	return tmpl.ParseFS(tmplDir, "email_*")
+}
